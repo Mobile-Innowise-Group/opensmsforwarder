0c8d1d94a02a7857e4653428d448b48cfebcd4f7
Denis Zabalotski <denis.zabolotsky@innowise.com>
2024-09-11 17:14:47 +0400

    Added firebase analytic

--- app/src/main/kotlin/com/github/opensmsforwarder/analytics/FirebaseTracker.kt
+++ app/src/main/kotlin/com/github/opensmsforwarder/analytics/FirebaseTracker.kt
@@ -2,11 +2,15 @@ package com.github.opensmsforwarder.analytics
 
 import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
+import java.text.SimpleDateFormat
+import java.util.Locale
 import javax.inject.Inject
 
 class FirebaseTracker @Inject constructor(
     private val firebaseAnalytics: FirebaseAnalytics
 ) : AnalyticsTracker {
+
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
 
     override fun trackEvent(event: String) {
         val params = Bundle()
@@ -25,7 +29,7 @@ class FirebaseTracker @Inject constructor(
     }
 
     private fun logEventWithTimestamp(event: String, params: Bundle) {
-        params.putLong(TIMESTAMP_KEY, System.currentTimeMillis())
+        params.putString(TIMESTAMP_KEY, dateFormat.format(System.currentTimeMillis()))
         firebaseAnalytics.logEvent(event, params)
     }
 
