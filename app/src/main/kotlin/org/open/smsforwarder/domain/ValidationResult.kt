495ce0cd4a6ac5c631790ae105cbb42740cc9a57
Dmitriy <dpmorskoy@gmail.com>
2025-02-19 12:27:42 +0300

    Fix lint issues

--- app/src/main/kotlin/org/open/smsforwarder/domain/ValidationResult.kt
+++ app/src/main/kotlin/org/open/smsforwarder/domain/ValidationResult.kt
@@ -2,7 +2,6 @@ package org.open.smsforwarder.domain
 
 import org.open.smsforwarder.utils.Resources
 
-// TODO: Ticket 14. Refactor this entity and use cases - move errorMessage out of the domain.
 data class ValidationResult(
     val successful: Boolean,
     val errorMessage: Resources.StringProvider? = null
